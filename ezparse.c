ccbb11b4096c804be932c656f638d73d58181565
Steven A. Falco <stevenfalco@gmail.com>
2022-02-13 15:08:06 -0500

    Update copyright dates

--- ezparse.c
+++ ezparse.c
@@ -1,4 +1,4 @@
-// Copyright 2020 Steven A. Falco <stevenfalco@gmail.com>
+// Copyright 2020,2022 Steven A. Falco <stevenfalco@gmail.com>
 //
 //  ezparse is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
